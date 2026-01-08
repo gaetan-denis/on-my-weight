cab20ac05f00d44bff1e42ac7a2b4bfce0efc430
Gaëtan Denis <gaetan.denis@outlook.be>
2026-01-08 15:55:59 +0100

    feat(auth): add AuthService for login, logout and register

---
+++ src/services/AuthService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OnTheWeight\services;
+
+use DateTime;
+use DateTimeImmutable;
+use OnTheWeight\repositories\UserRepository;
+use OnTheWeight\entities\User;
+use RuntimeException;
+
+class AuthService
+{
+    public function __construct(private UserRepository $repo)
+    {
+    }
+    public function login(string $email, string $password): void
+    {
+        $user = $this->repo->getUserByEmail($email);
+        if (!$user->verifyPassword($password)) {
+            throw new RunTimeException("Invalid credentials.");
+        }
+        session_start();
+        $_SESSION['user_id'] = $user->getId();
+    }
+
+    public function logout(): void
+    {
+        session_start();
+        session_abort();
+        session_destroy();
+    }
+
+    public function register(string $username, string $email, string $password)
+    {
+        $user = new User(
+            0,
+            new DateTimeImmutable(),
+            new DateTime(),
+            $username,
+            $email,
+            $password
+        );
+        $user->setPassword($password);
+        $this->repo->addUser($user->getUsername(), $user->getEmail(), $user->getPasswordHash())
+    }
+}
